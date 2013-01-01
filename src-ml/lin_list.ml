14be892f71349376b171fc40e283d17d801232ca
yshl <yshl@takechiyo.net>
2013-01-01 23:14:16 +0900

    modify some functions to tail rec

--- src-ml/lin_list.ml
+++ src-ml/lin_list.ml
@@ -43,28 +43,31 @@ module Lin_list = struct
                 newaijs, newbi;;
 
     let elim_col abjs aij bi =
-        List.map (fun abj ->
+        List.rev_map (fun abj ->
             match abj with
             | [], _ -> failwith "invalid arg"
             | aji::ajj, bj -> (listsub ajj aij aji),(bj -. aji *. bi)
             ) abjs;;
 
-    let rec forward_elimination ab=
-        let ab = pivot ab in
-        match ab with
-        | [] -> []
-        | abi::abtl ->
-                let aijs, bi=scale_pivot abi in
-                let newab = elim_col abtl aijs bi in
-                (aijs, bi)::(forward_elimination newab);;
+    let forward_elimination ab =
+        let rec forward_rec ab acc=
+            let ab = pivot ab in
+            match ab with
+            | [] -> acc
+            | abi::abtl ->
+                    let aijs, bi=scale_pivot abi in
+                    let newab = elim_col abtl aijs bi in
+                    forward_rec newab ((aijs, bi)::acc)  in
+        forward_rec ab [];;
 
-    let rec back_substitution ab=
-        match ab with
-        | [] -> []
-        | (aij, bi)::abjj ->
-                let bj = back_substitution abjj in
-                let newb = bi -. (prod aij bj) in
-                newb::bj;;
+    let back_substitution ab =
+        let rec back_rec ab bj=
+            match ab with
+            | [] -> bj
+            | (aij, bi)::abjj ->
+                    let newb = bi -. (prod aij bj) in
+                    back_rec abjj (newb::bj) in
+        back_rec ab [];;
 
     let solve a b=
         let ab=List.map2 (fun ai bi->(ai,bi)) a b in
