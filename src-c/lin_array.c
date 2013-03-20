f3cdd0578756d2038f4b6cd2cb52b57f718d874a
yshl <yshl@takechiyo.net>
2013-03-20 09:54:46 +0900

    modify: partial pivoting solver by C to some functions

--- src-c/lin_array.c
+++ src-c/lin_array.c
@@ -5,6 +5,26 @@
 
 #define swap(type, a, b) do{type tmp=a; a=b; b=tmp;}while(0)
 
+static void scale_matrix_row(int N, double a[N][N], double b[N])
+{
+    int i,j;
+    for(i=0; i<N; i++){
+	double aijmax=fabs(a[i][0]);
+	double factor;
+	for(j=1; j<N; j++){
+	    double aij=fabs(a[i][j]);
+	    if(aij>aijmax){
+		aijmax=aij;
+	    }
+	}
+	factor=1.0/aijmax;
+	for(j=0; j<N; j++){
+	    a[i][j]*=factor;
+	}
+	b[i]*=factor;
+    }
+}
+
 static void pivot(int N, double a[N][N], double b[N], int i)
 {
     double ajimax=fabs(a[i][i]);
@@ -25,48 +45,52 @@ static void pivot(int N, double a[N][N], double b[N], int i)
     }
 }
 
-void solve(int N, double a[N][N], double b[N])
+static void update_upper_row(int N, double a[N][N], double b[N], int i)
 {
-    int i,j,k;
+    int j;
+    double factor;
+    factor=1.0/a[i][i];
+    for(j=i+1; j<N; j++){
+	a[i][j]*=factor;
+    }
+    b[i]*=factor;
+}
 
-    /* scale */
-    for(i=0; i<N; i++){
-	double aijmax=fabs(a[i][0]);
+static void forward_elimination(int N, double a[N][N], double b[N], int i)
+{
+    int j,k;
+    for(j=i+1; j<N; j++){
 	double factor;
-	for(j=1; j<N; j++){
-	    double aij=fabs(a[i][j]);
-	    if(aij>aijmax){
-		aijmax=aij;
-	    }
+	factor=a[j][i];
+	for(k=i+1; k<N; k++){
+	    a[j][k]-=factor*a[i][k];
 	}
-	factor=1.0/aijmax;
-	for(j=0; j<N; j++){
-	    a[i][j]*=factor;
-	}
-	b[i]*=factor;
+	b[j]-=factor*b[i];
     }
-    for(i=0; i<N; i++){
-	double factor;
-	/* pivot */
-	pivot(N,a,b,i);
-	/* forward */
-	factor=1.0/a[i][i];
-	for(j=i+1; j<N; j++){
-	    a[i][j]*=factor;
-	}
-	b[i]*=factor;
-	for(j=i+1; j<N; j++){
-	    factor=a[j][i];
-	    for(k=i+1; k<N; k++){
-		a[j][k]-=factor*a[i][k];
-	    }
-	    b[j]-=factor*b[i];
-	}
-    }
-    /* back */
+}
+
+static void backward_substitution(int N, double a[N][N], double b[N])
+{
+    int i,j;
     for(i=N-1; i>=0; i--){
 	for(j=i+1; j<N; j++){
 	    b[i]-=a[i][j]*b[j];
 	}
     }
+}
+
+void solve(int N, double a[N][N], double b[N])
+{
+    int i;
+    /* scale */
+    scale_matrix_row(N,a,b);
+    for(i=0; i<N; i++){
+	/* pivot */
+	pivot(N,a,b,i);
+	/* forward */
+	update_upper_row(N,a,b,i);
+	forward_elimination(N,a,b,i);
+    }
+    /* back */
+    backward_substitution(N,a,b);
 }
--- src-c/lin_pointer.c
+++ src-c/lin_pointer.c
@@ -5,6 +5,26 @@
 
 #define swap(type, a, b) do{type tmp=a; a=b; b=tmp;}while(0)
 
+static void scale_matrix_row(double** a, double* b, int N)
+{
+    int i,j;
+    for(i=0; i<N; i++){
+	double aijmax=fabs(a[i][0]);
+	double factor;
+	for(j=1; j<N; j++){
+	    double aij=fabs(a[i][j]);
+	    if(aij>aijmax){
+		aijmax=aij;
+	    }
+	}
+	factor=1.0/aijmax;
+	for(j=0; j<N; j++){
+	    a[i][j]*=factor;
+	}
+	b[i]*=factor;
+    }
+}
+
 static void pivot(double** a, double* b, int N, int i)
 {
     double ajimax=fabs(a[i][i]);
@@ -23,48 +43,51 @@ static void pivot(double** a, double* b, int N, int i)
     }
 }
 
-void solve(double **a, double *b, int N)
+static void update_upper_row(double** a, double* b, int N, int i)
 {
-    int i,j,k;
+    int j;
+    double factor;
+    factor=1.0/a[i][i];
+    for(j=i+1; j<N; j++){
+	a[i][j]*=factor;
+    }
+    b[i]*=factor;
+}
 
-    /* scale */
-    for(i=0; i<N; i++){
-	double aijmax=fabs(a[i][0]);
-	double factor;
-	for(j=1; j<N; j++){
-	    double aij=fabs(a[i][j]);
-	    if(aij>aijmax){
-		aijmax=aij;
-	    }
+static void forward_elimination(double** a, double* b, int N, int i)
+{
+    int j,k;
+    for(j=i+1; j<N; j++){
+	double factor=a[j][i];
+	for(k=i+1; k<N; k++){
+	    a[j][k]-=factor*a[i][k];
 	}
-	factor=1.0/aijmax;
-	for(j=0; j<N; j++){
-	    a[i][j]*=factor;
-	}
-	b[i]*=factor;
+	b[j]-=factor*b[i];
     }
-    for(i=0; i<N; i++){
-	double factor;
-	/* pivot */
-	pivot(a,b,N,i);
-	/* forward */
-	factor=1.0/a[i][i];
-	for(j=i+1; j<N; j++){
-	    a[i][j]*=factor;
-	}
-	b[i]*=factor;
-	for(j=i+1; j<N; j++){
-	    factor=a[j][i];
-	    for(k=i+1; k<N; k++){
-		a[j][k]-=factor*a[i][k];
-	    }
-	    b[j]-=factor*b[i];
-	}
-    }
-    /* back */
+}
+
+static void back_substitution(double** a, double* b, int N)
+{
+    int i,j;
     for(i=N-1; i>=0; i--){
 	for(j=i+1; j<N; j++){
 	    b[i]-=a[i][j]*b[j];
 	}
     }
+}
+
+void solve(double **a, double *b, int N)
+{
+    int i;
+    /* scale */
+    scale_matrix_row(a,b,N);
+    for(i=0; i<N; i++){
+	/* pivot */
+	pivot(a,b,N,i);
+	/* forward */
+	update_upper_row(a,b,N,i);
+        forward_elimination(a,b,N,i);
+    }
+    /* back */
+    back_substitution(a,b,N);
 }
