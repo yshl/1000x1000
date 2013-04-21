d5fa225710d23b664a552c2d75a1f17a76e5c65c
yshl <yshl@takechiyo.net>
2013-04-21 17:17:13 +0900

    refactoring lin.scm

--- src-scm/test_1.scm
+++ src-scm/test_1.scm
@@ -1,17 +1,24 @@
 (module test
 	(import (lin "./lin.scm")))
+
 (define n 1000)
-(define a (make-vector n))
-(foreach 0 n
-	 (lambda (i)
-	   (let ((ai (make-f64vector n 1.0)))
-	     (f64vector-set! ai i 1001.0)
-	     (vector-set! a i ai))))
+(define a
+  (vector-map
+    (lambda(i)
+      (let ((ai (make-f64vector n 1.0)))
+	(f64vector-set! ai i 1001.0)
+	ai))
+    (list->vector (iota n))))
 (define b (make-f64vector n 1000.0))
+
 (solve a b)
+
+(define (absmax a b)
+  (if (not (<= (abs a) (abs b)))
+    a b))
 (display
-  (foreach-fold 0 n 0.0
-		(lambda (i errmax)
-		  (let ((bi-error (- (f64vector-ref b i) 0.5)))
-		    (if (not (<= (abs bi-error) (abs errmax))) bi-error errmax)))))
+  (let loop ((errmax 0.0) (i 0))
+    (if (< i n)
+      (loop (absmax (- (f64vector-ref b i) 0.5) errmax) (+ i 1))
+      errmax)))
 (newline)
