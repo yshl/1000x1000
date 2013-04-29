2959820ef9978065ad7067c32bc6a73546cca20a
yshl <yshl@takechiyo.net>
2013-04-29 18:26:14 +0900

    modify lin.scm, use srfi-43 like procedure name

--- src-scm/test_1.scm
+++ src-scm/test_1.scm
@@ -1,24 +1,30 @@
-(module test
-	(import (lin "./lin.scm")))
+(cond-expand
+  (bigloo (module test
+		  (import (lin "./lin.scm"))))
+  (chicken (begin
+	     (use srfi-1)
+	     (use srfi-4)
+	     (use vector-lib))))
 
 (define n 1000)
-(define a
-  (vector-map
-    (lambda(i)
-      (let ((ai (make-f64vector n 1.0)))
-	(f64vector-set! ai i 1001.0)
-	ai))
-    (list->vector (iota n))))
+(define a (make-vector n))
+(let loop ((i 0))
+  (when (< i n)
+    (let ((ai (make-f64vector n 1.0)))
+      (f64vector-set! ai i 1001.0)
+      (vector-set! a i ai))
+    (loop (+ i 1))))
+
 (define b (make-f64vector n 1000.0))
 
 (solve a b)
 
-(define (absmax a b)
+(define (amax a b)
   (if (not (<= (abs a) (abs b)))
     a b))
 (display
   (let loop ((errmax 0.0) (i 0))
     (if (< i n)
-      (loop (absmax (- (f64vector-ref b i) 0.5) errmax) (+ i 1))
+      (loop (amax (- (f64vector-ref b i) 0.5) errmax) (+ i 1))
       errmax)))
 (newline)
