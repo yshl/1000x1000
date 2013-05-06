8c9941be4fe04a3f1390229082d737bb78ecb49f
yshl <yshl@takechiyo.net>
2013-05-06 22:52:08 +0900

    modify: lin_dynamic.d use block LU

--- src-d/lin_dynamic.d
+++ src-d/lin_dynamic.d
@@ -19,26 +19,65 @@ private void pivot(double[][] a, double[] b, size_t i)
     }
 }
 
+private void update_lower_col(double[][] a, double[] b, size_t i, size_t blockend)
+{
+    foreach(i1; i..blockend){
+	pivot(a,b,i1);
+	double factor=1.0/a[i1][i1];
+	a[i1][i1+1..blockend]*=factor;
+	b[i1]*=factor;
+	foreach(j; i1+1..a.length){
+	    factor=a[j][i1];
+	    a[j][i1+1..blockend]-=factor*a[i1][i1+1..blockend];
+	    b[j]-=factor*b[i1];
+	}
+    }
+}
+
+private void update_upper_row(double[][] a, size_t i, size_t blockend)
+{
+    foreach(i1; i..blockend){
+	double factor=1.0/a[i1][i1];
+	a[i1][blockend..$]*=factor;
+	foreach(j; i1+1..blockend){
+	    a[j][blockend..$]-=a[j][i1]*a[i1][blockend..$];
+	}
+    }
+}
+
+private void forward_elimination(double[][] a, size_t i, size_t blockend)
+{
+    size_t n=a.length;
+    size_t blocksize=32;
+    for(size_t j=blockend; j<n; j+=blocksize){
+	size_t jend=min(j+blocksize, n);
+	for(size_t k=blockend; k<n; k+=blocksize){
+	    size_t kend=min(k+blocksize, n);
+	    foreach(j1; j..jend){
+		foreach(l; i..blockend){
+		    a[j1][k..kend]-=a[j1][l]*a[l][k..kend];
+		}
+	    }
+	}
+    }
+}
+
 void solve(double[][] a, double[] b)
 {
     /* scale */
     foreach(i, ref ai; a){
-        double factor=1.0/reduce!("fmax(a,fabs(b))")(0.0,ai);
+	double factor=1.0/reduce!("fmax(a,fabs(b))")(0.0,ai);
 	ai[]*=factor;
 	b[i]*=factor;
     }
-    foreach(i; 0..a.length){
-	/* pivot */
-	pivot(a,b,i);
-	/* forward */
-	double factor=1.0/a[i][i];
-	a[i][i+1..$]*=factor;
-	b[i]*=factor;
-	foreach(j; i+1..a.length){
-	    factor=a[j][i];
-	    a[j][i+1..$]-=factor*a[i][i+1..$];
-	    b[j]-=factor*b[i];
-	}
+    /* forward */
+    size_t n=a.length;
+    size_t blocksize=32;
+    for(size_t i=0; i<n; i+=blocksize){
+	size_t blockend=min(i+blocksize,n);
+	update_lower_col(a,b,i,blockend);
+	update_upper_row(a,i,blockend);
+	forward_elimination(a,i,blockend);
     }
     /* back */
     foreach_reverse(i, ai; a){
