a7065de4271f5a749ac8e61deb62da7ede85e80f
yshl <yshl@takechiyo.net>
2013-06-09 13:24:25 +0900

    optimize lin_dynamic.d

--- src-d/lin_dynamic.d
+++ src-d/lin_dynamic.d
@@ -49,15 +49,15 @@ private void update_upper_row(double[][] a, size_t i, size_t blockend)
 
 private void forward_elimination(double[][] a, size_t i, size_t blockend)
 {
-    size_t n=a.length;
     const size_t blocksize=4;
-    foreach(ref aj; a[blockend..n]){
-	size_t l=i;
-	for(; l+blocksize<=blockend; l+=blocksize){
-	    aj[blockend..$]-=aj[l+0]*a[l+0][blockend..$]
-	        +aj[l+1]*a[l+1][blockend..$]
-	        +aj[l+2]*a[l+2][blockend..$]
-	        +aj[l+3]*a[l+3][blockend..$];
+    foreach(ref aj; a[blockend..$]){
+	size_t l;
+	for(l=i; l+blocksize<=blockend; l+=blocksize){
+	    double ajl0=aj[l+0],ajl1=aj[l+1],ajl2=aj[l+2],ajl3=aj[l+3];
+	    foreach(k; blockend..aj.length){
+		aj[k]-=ajl0*a[l+0][k]+ajl1*a[l+1][k]
+		    +ajl2*a[l+2][k]+ajl3*a[l+3][k];
+	    }
 	}
 	for(; l<blockend; l++){
             aj[blockend..$]-=aj[l]*a[l][blockend..$];
